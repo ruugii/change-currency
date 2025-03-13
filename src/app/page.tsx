7161a8d1ed6ae03f5faf24bf3b62aed0cd0aa875
ruugii_31 <roger.barrero.sorribas@gmail.com>
2025-03-13 15:32:51 +0100

    add GitHub logo and footer to the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,18 @@ import ChangeCurrency from "./components/ChangeCurrency/index";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div/>
+      <div />
       <ChangeCurrency />
-      <div>
-        footer
-      </div>
+      <footer className=" flex w-full content-center justify-center items-center gap-4">
+        <p className=" flex gap-2.5">
+          Created by <a href="https://github.com/ruugii" className=" flex content-center justify-center items-center gap-2.5 hover:underline">Roger Barrero</a>
+        </p>
+        <p className=" flex gap-2.5">
+          <a href="https://github.com/ruugii/change-currency" className=" flex content-center justify-center items-center gap-2.5 hover:underline">
+            Github repository <img src="/github-logo.svg" alt="github logo" width="20px" />
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
